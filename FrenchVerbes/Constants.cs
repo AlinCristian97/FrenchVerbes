62033a4229b425cb100b6cb2559b0c0d04b203ad
Alin <alin.cristian977@gmail.com>
2026-03-19 21:38:14 +0200

    Update Constants.cs

--- FrenchVerbes/Constants.cs
+++ FrenchVerbes/Constants.cs
@@ -404,6 +404,7 @@ public static class Constants
                 public const string Dresser = "dresser";
 
                 // E
+                public const string Envisager = "envisager";
                 public const string Exiger = "exiger";
                 public const string Engueuler = "engueuler";
                 public const string Ausculter = "ausculter";
