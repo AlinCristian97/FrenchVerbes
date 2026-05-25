06a13137905a535ea03896c10e8e7f74b47614a7
Alin <alin.cristian977@gmail.com>
2026-05-25 21:16:43 +0200

    ir repo shells

---
+++ FrenchVerbes/Verbes/AllVerbesRepository/Regular/Ir/FrenchVerbesRepository.Regular.Ir.B.cs
@@ -0,0 +1,18 @@
+using FrenchVerbes.AllConstants;
+using FrenchVerbes.Verbes.Irregular;
+
+namespace FrenchVerbes.Verbes;
+
+public static partial class VerbRepository
+{
+    private static void AddRegularIrBVerbs()
+    {
+
+    }
+
+    #region FactoryMethods
+
+
+
+    #endregion
+}
