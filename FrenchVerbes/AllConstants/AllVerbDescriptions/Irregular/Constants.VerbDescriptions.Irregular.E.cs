029a02943f5a6755411c3416493446dcff3f239c
Alin <alin.cristian977@gmail.com>
2026-05-25 20:06:55 +0200

    e

--- FrenchVerbes/AllConstants/AllVerbDescriptions/Irregular/Constants.VerbDescriptions.Irregular.E.cs
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Irregular/Constants.VerbDescriptions.Irregular.E.cs
@@ -6,7 +6,7 @@ public static partial class Constants
     {
         public static partial class Irregular
         {
-
+            public const string Etre = "Être est le verbe le plus fondamental. Il exprime l'état, l'identité et sert d'auxiliaire pour de nombreux temps.";
         }
     }
 }
--- FrenchVerbes/AllConstants/Constants.cs
+++ FrenchVerbes/AllConstants/Constants.cs
@@ -8,24 +8,6 @@ public static partial class Constants
 
         public static partial class Irregular
         {
-            // E
-            public const string Echoir = "échoir";
-            public const string Etendre = "étendre";
-            public const string Se_Etendre = "s'étendre";
-            public const string Enduire = "enduire";
-            public const string Ecrire = "écrire";
-            public const string Etre = "être";
-            public const string Eteindre = "éteindre";
-            public const string Exclure = "exclure";
-            public const string Elimer = "élimer";
-            public const string Emmener = "emmener";
-            public const string Emettre = "émettre";
-            public const string Entrevoir = "entrevoir";
-            public const string Entreprendre = "entreprendre";
-            public const string Se_Enfuir = "s'enfuir";
-            public const string Enfouir = "enfouir";
-            public const string Se_Enfouir = "s'enfouir";
-
             // F
             public const string Faire = "faire";
             public const string Fendre = "fendre";
