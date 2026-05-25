144054899cc1507470cd0103e9b53e8c688b6987
Alin <alin.cristian977@gmail.com>
2026-05-25 21:14:35 +0200

    shells

---
+++ FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.H.cs
@@ -0,0 +1,18 @@
+using FrenchVerbes.AllConstants;
+using FrenchVerbes.Verbes.Irregular;
+
+namespace FrenchVerbes.Verbes;
+
+public static partial class VerbRepository
+{
+    private static void AddRegularErHVerbs()
+    {
+
+    }
+
+    #region FactoryMethods
+
+
+
+    #endregion
+}
