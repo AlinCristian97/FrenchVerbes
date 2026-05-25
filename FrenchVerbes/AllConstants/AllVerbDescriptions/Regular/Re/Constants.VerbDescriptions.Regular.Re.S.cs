74156500b8f794dfa5756248ce7025991662e170
Alin <alin.cristian977@gmail.com>
2026-05-25 20:00:46 +0200

    shells

---
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Re/Constants.VerbDescriptions.Regular.Re.S.cs
@@ -0,0 +1,15 @@
+namespace FrenchVerbes.AllConstants;
+
+public static partial class Constants
+{
+    public static partial class VerbDescriptions
+    {
+        public static partial class Regular
+        {
+            public static partial class Re
+            {
+
+            }
+        }
+    }
+}
