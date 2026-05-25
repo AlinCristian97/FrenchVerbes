15960db571a3a67da4dce6b69975a15c8ce916ef
Alin <alin.cristian977@gmail.com>
2026-05-25 19:47:41 +0200

    shells

---
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Irregular/Constants.VerbDescriptions.Irregular.X.cs
@@ -0,0 +1,12 @@
+namespace FrenchVerbes.AllConstants;
+
+public static partial class Constants
+{
+    public static partial class VerbDescriptions
+    {
+        public static partial class Irregular
+        {
+
+        }
+    }
+}
