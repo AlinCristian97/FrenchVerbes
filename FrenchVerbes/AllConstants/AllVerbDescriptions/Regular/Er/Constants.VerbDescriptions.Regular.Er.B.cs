b4abba2227384cfec15e00388864c47a46e30591
Alin <alin.cristian977@gmail.com>
2026-05-26 02:35:24 +0200

    bénéficier

--- FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.B.cs
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.B.cs
@@ -8,7 +8,7 @@ public static partial class Constants
         {
             public static partial class Er
             {
-
+                public const string Beneficier = "Bénéficier est un verbe régulier du premier groupe (-er) qui signifie procurer un avantage, apporter un bienfait ou tirer profit de quelque chose.";
             }
         }
     }
--- FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.B.cs
+++ FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.B.cs
@@ -1,4 +1,5 @@
 using FrenchVerbes.AllConstants;
+using FrenchVerbes.Verbes.Regular;
 
 namespace FrenchVerbes.Verbes;
 
@@ -6,12 +7,17 @@ public static partial class VerbRepository
 {
     private static void AddRegularErBVerbs()
     {
-
+        _verbs.Add(CreateBeneficier());
     }
 
     #region FactoryMethods
 
-
+    private static RegularErVerb CreateBeneficier()
+    {
+        return new RegularErVerb(
+            Constants.Verbs.Regular.Er.Beneficier,
+            Constants.VerbDescriptions.Regular.Er.Beneficier);
+    }
 
     #endregion
 }
