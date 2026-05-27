32b5fb3c8c688c4b4388f367e2b28ec6e3f50016
Alin <alin.cristian977@gmail.com>
2026-05-27 13:41:01 +0300

    Update Constants.Verbs.Regular.Er.A.cs

--- FrenchVerbes/AllConstants/AllVerbs/Regular/Er/Constants.Verbs.Regular.Er.A.cs
+++ FrenchVerbes/AllConstants/AllVerbs/Regular/Er/Constants.Verbs.Regular.Er.A.cs
@@ -37,8 +37,8 @@ public static partial class Constants
                 public const string Se_Approcher = "s'approcher";
                 public const string Ajouter = "ajouter";
                 public const string Abandonner = "abandonner";
-                public const string Annuler = "annuler"; // <-- you are here
-                public const string Acheter = "acheter";
+                public const string Annuler = "annuler";
+                public const string Acheter = "acheter"; // <-- you are here
                 public const string Activer = "activer";
                 public const string Adapter = "adapter";
                 public const string Se_Adapter = "s'adapter";
