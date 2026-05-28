4dfb7fdc32927cbe7c53175cec31917eb0cb4efc
Alin <alin.cristian977@gmail.com>
2026-05-28 13:09:41 +0300

    Update Constants.Verbs.Regular.Er.A.cs

--- FrenchVerbes/AllConstants/AllVerbs/Regular/Er/Constants.Verbs.Regular.Er.A.cs
+++ FrenchVerbes/AllConstants/AllVerbs/Regular/Er/Constants.Verbs.Regular.Er.A.cs
@@ -43,8 +43,8 @@ public static partial class Constants
                 public const string Adapter = "adapter";
                 public const string Se_Adapter = "s'adapter";
                 public const string Aimer = "aimer";
-                public const string Appeler = "appeler"; // <-- you are here
-                public const string Arriver = "arriver";
+                public const string Appeler = "appeler";
+                public const string Arriver = "arriver"; // <-- you are here
                 public const string Attraper = "attraper";
                 public const string Amuser = "amuser";
                 public const string Se_Amuser = "s'amuser";
