a980105cdccfda192f6e80184ff99d04889d52c6
Alin <alin.cristian977@gmail.com>
2026-05-30 20:45:38 +0200

    i

--- FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.I.cs
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.I.cs
@@ -8,7 +8,7 @@ public static partial class Constants
         {
             public static partial class Er
             {
-
+                public const string Imposer = "Imposer signifie obliger quelqu'un à accepter quelque chose ou à faire quelque chose, ou exercer une autorité sur quelqu'un. C'est un verbe régulier du premier groupe en -er.";
             }
         }
     }
--- FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.I.cs
+++ FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.I.cs
@@ -1,4 +1,5 @@
 using FrenchVerbes.AllConstants;
+using FrenchVerbes.Verbes.Regular;
 
 namespace FrenchVerbes.Verbes;
 
@@ -6,12 +7,17 @@ public static partial class VerbRepository
 {
     private static void AddRegularErIVerbs()
     {
-
+        _verbs.Add(CreateImposer());
     }
 
     #region FactoryMethods
 
-
+    private static RegularErVerb CreateImposer()
+    {
+        return new RegularErVerb(
+            Constants.Verbs.Regular.Er.Imposer,
+            Constants.VerbDescriptions.Regular.Er.Imposer);
+    }
 
     #endregion
 }
