b4936d81b4573698c8933701808491bfefffca29
Alin <alin.cristian977@gmail.com>
2026-05-30 02:54:27 +0200

    e

--- FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.E.cs
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.E.cs
@@ -8,7 +8,7 @@ public static partial class Constants
         {
             public static partial class Er
             {
-
+                public const string Envisager = "Envisager signifie considérer quelque chose comme possible ou probable, imaginer une situation future ou envisager une possibilité. C'est un verbe régulier du premier groupe en -er.";
             }
         }
     }
--- FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.E.cs
+++ FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.E.cs
@@ -1,4 +1,5 @@
 using FrenchVerbes.AllConstants;
+using FrenchVerbes.Verbes.Regular;
 
 namespace FrenchVerbes.Verbes;
 
@@ -6,12 +7,17 @@ public static partial class VerbRepository
 {
     private static void AddRegularErEVerbs()
     {
-
+        _verbs.Add(CreateEnvisager());
     }
 
     #region FactoryMethods
 
-
+    private static RegularErVerb CreateEnvisager()
+    {
+        return new RegularErVerb(
+            Constants.Verbs.Regular.Er.Envisager,
+            Constants.VerbDescriptions.Regular.Er.Envisager);
+    }
 
     #endregion
 }
