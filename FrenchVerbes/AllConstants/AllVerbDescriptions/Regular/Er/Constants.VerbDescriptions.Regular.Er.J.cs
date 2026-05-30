a106134b9246ed625c01ed23d4bc0d63a3b77890
Alin <alin.cristian977@gmail.com>
2026-05-31 00:36:09 +0200

    j

--- FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.J.cs
+++ FrenchVerbes/AllConstants/AllVerbDescriptions/Regular/Er/Constants.VerbDescriptions.Regular.Er.J.cs
@@ -8,7 +8,7 @@ public static partial class Constants
         {
             public static partial class Er
             {
-
+                public const string Joncher = "Joncher signifie couvrir une surface en y répandant des objets épars, comme des feuilles, des fleurs ou des débris. C'est un verbe régulier du premier groupe en -er.";
             }
         }
     }
--- FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.J.cs
+++ FrenchVerbes/Verbes/AllVerbesRepository/Regular/Er/FrenchVerbesRepository.Regular.Er.J.cs
@@ -1,4 +1,5 @@
 using FrenchVerbes.AllConstants;
+using FrenchVerbes.Verbes.Regular;
 
 namespace FrenchVerbes.Verbes;
 
@@ -6,12 +7,17 @@ public static partial class VerbRepository
 {
     private static void AddRegularErJVerbs()
     {
-
+        _verbs.Add(CreateJoncher());
     }
 
     #region FactoryMethods
 
-
+    private static RegularErVerb CreateJoncher()
+    {
+        return new RegularErVerb(
+            Constants.Verbs.Regular.Er.Joncher,
+            Constants.VerbDescriptions.Regular.Er.Joncher);
+    }
 
     #endregion
 }
